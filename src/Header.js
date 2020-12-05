c45335e599f9ae1f129411d25faceb4516ff1514
Daniel Woelfel <dwwoelfel@gmail.com>
2020-12-04 17:47:48 -0800

    fix rebase

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import {Box, Heading} from 'grommet';
+import {Box, Heading, Text} from 'grommet';
 import Avatar from './Avatar';
 import Link from 'next/link';
 import ConfigContext from './ConfigContext';
